44edfd6e8080c514e52f4d9ecb99b9e26af0e9f4
huangjxbq <519207704@qq.com>
2020-02-20 18:03:38 +0800

    更改Font

--- Learn/Font/Widget.cpp
+++ Learn/Font/Widget.cpp
@@ -1,8 +1,26 @@
 #include "Widget.h"
 #include <QFont>
+#include <QDebug>
 
 Widget::Widget(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+
+
+	QFont font;
+
+
+	font.setFamily(u8"Î¢ÈíÑÅºÚ");
+	font.setPointSize(4);
+	//font.setPixelSize(50);
+	//font.setWeight(QFont::Black);
+
+
+	ui.label->setFont(font);
+	ui.label->resize(100, 50);
+	ui.label->setText(u8"ÄãºÃ");
+	ui.label->update();
+
+	qDebug() << font;
 }
