cdef46808b234715eb0d801a524b6021bd094a0d
huangjxbq <519207704@qq.com>
2020-02-25 18:27:22 +0800

    添加FontChange

--- Learn/Font/Widget.h
+++ Learn/Font/Widget.h
@@ -10,9 +10,28 @@ class Widget : public QWidget
 
 public:
 	Widget(QWidget *parent = Q_NULLPTR);
+	~Widget();
+
+
+
+private slots:
+	//Qt安装卸载字体
+	void on_pushButton_Browse_Qt_clicked();  //选择文件
+
+	void on_pushButton_Install_Qt_clicked();  //安装字体
+
+
 
 private:
-	void EnumFont();
+	void EnumFontUseQt();    //Qt枚举字体
+
+	void InstallFontUseQt(const QStringList &files);  //Qt安装字体
+
+	void EnumFontUseWin32();
+
+	void InstallFontUseWin32();  //Win32安装字体
+
+	void RemoveFontUseWin32();   //Win32卸载字体
 
 	static int CALLBACK CallBackEnumFontFamExProc(
 		const LOGFONT    *lpelfe,
@@ -23,4 +42,11 @@ private:
 
 
 	Ui::WidgetClass ui;
+
+
+	QMap<QString, int> m_InstalledFontFile; //!<-已经安装的字体文件
+
+	int m_win_font_1;
+	int m_win_font_2;
+	int m_win_font_3;
 };
---
+++ Learn/FontChange/FontChange.h
@@ -0,0 +1,32 @@
+﻿
+// FontChange.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CFontChangeApp:
+// 有关此类的实现，请参阅 FontChange.cpp
+//
+
+class CFontChangeApp : public CWinApp
+{
+public:
+	CFontChangeApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFontChangeApp theApp;
