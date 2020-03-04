f7f18279e7ab8be3aef52fa01e4a14387b0609d1
huangjxbq <519207704@qq.com>
2020-03-04 18:34:23 +0800

    ss

--- Learn/Movie/Widget.cpp
+++ Learn/Movie/Widget.cpp
@@ -1,5 +1,6 @@
 #include "Widget.h"
 #include <QMovie>
+#include <QStyle>
 #include <QDebug>
 
 Widget::Widget(QWidget *parent)
@@ -8,8 +9,15 @@ Widget::Widget(QWidget *parent)
 	ui.setupUi(this);
 
 
+	ui.label->setBackgroundRole(QPalette::Dark);
 
-	//QMovie movie;
+	//ÉèÖÃ°´Å¥
+	ui.toolButton_open->setIcon(style()->standardIcon(QStyle::SP_DialogOpenButton));
+	ui.toolButton_start->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+	ui.toolButton_pause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+	ui.toolButton_stop->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
+
+	QMovie movie;
 
 	qDebug() << QMovie::supportedFormats();
 }
