9ba4d3aea9c41d00c97da0156a497fe89bdb615b
huangjxbq <519207704@qq.com>
2020-03-06 18:04:54 +0800

    ITK-Version

---
+++ Learn/VersionAndBuildInfomation/VersionAndBuildInfomation.cpp
@@ -0,0 +1,65 @@
+﻿#include <itkVersion.h>
+#include <itkBuildInformation.h>
+
+
+void Version();
+
+void BuildInformation();
+
+int main()
+{
+	Version();
+
+	BuildInformation();
+	
+	system("pause");
+}
+
+void Version()
+{
+	std::cout << "======================itkVersion======================" << std::endl;
+
+	//5个静态函数
+	std::cout << "************************静态函数***********************" << std::endl;
+	std::cout << "ITKVersion:" << itk::Version::GetITKVersion() << std::endl;
+	std::cout << "ITKMajorVersion:" << itk::Version::GetITKMajorVersion() << std::endl;
+	std::cout << "ITKMinorVersion:" << itk::Version::GetITKMinorVersion() << std::endl;
+	std::cout << "ITKBuildVersion:" << itk::Version::GetITKBuildVersion() << std::endl;
+	std::cout << "ITKSourceVersion:" << itk::Version::GetITKSourceVersion() << std::endl;
+
+	// 通过宏
+	std::cout << "**************************宏***************************" << std::endl;
+	std::cout << "ITK_VERSION:" << ITK_VERSION << std::endl;
+	std::cout << "ITK_VERSION_MAJOR:" << ITK_VERSION_MAJOR << std::endl;
+	std::cout << "ITK_VERSION_MINOR:" << ITK_VERSION_MINOR << std::endl;
+	std::cout << "ITK_VERSION_PATCH:" << ITK_VERSION_PATCH << std::endl;
+	std::cout << "ITK_SOURCE_VERSION:" << ITK_SOURCE_VERSION << std::endl;
+	std::cout << "ITK_VERSION_STRING:" << ITK_VERSION_STRING << std::endl;
+}
+
+std::string getkey()
+{
+	return "sss";
+}
+
+
+void BuildInformation()
+{
+	using BuildInformationMapType = itk::BuildInformation::MapType;
+	using MapKeyType = itk::BuildInformation::MapKeyType;
+
+
+	BuildInformationMapType map = itk::BuildInformation::GetMap();
+
+	std::vector<MapKeyType> keys = itk::BuildInformation::GetAllKeys();
+
+	for (MapKeyType key : keys)
+	{
+		
+		itk::BuildInformation::GetValue(getkey());
+
+		itk::BuildInformation::GetDescription(key);
+	}
+
+
+}
