ec21e9bd684dcd0a17f6e5269ea2623fd440b11d
huangjxbq <519207704@qq.com>
2020-03-19 18:50:17 +0800

    测试Win32DebugAppender

--- Learn/log4cplus/Widget.cpp
+++ Learn/log4cplus/Widget.cpp
@@ -1,6 +1,10 @@
 #include "Widget.h"
 #include <log4cplus/log4cplus.h>
-//#include <memory>
+#include <log4cplus/win32consoleappender.h>
+#include <log4cplus/win32debugappender.h>
+//#include <log4cplus/helpers/property.h>
+#include <Windows.h>
+
 
 using namespace log4cplus;
 
@@ -27,10 +31,23 @@ Widget::Widget(QWidget *parent)
 		LOG4CPLUS_TEXT("%d.log"), 2, false, true, false, false));
 	appender1->setName(LOG4CPLUS_TEXT("TimeBasedRollingFileAppender"));
 
-	log4cplus::SharedAppenderPtr appender2(new log4cplus::DailyRollingFileAppender(LOG4CPLUS_TEXT("test.log")
-		/*, log4cplus::MINUTELY, true, 10, false, false*//*, LOG4CPLUS_TEXT("yyyy-MM-dd")*/));
-	appender2->setName(LOG4CPLUS_TEXT("DailyRollingFileAppender"));
+	//log4cplus::SharedAppenderPtr appender2(new log4cplus::DailyRollingFileAppender(LOG4CPLUS_TEXT("test.log")
+	//	/*, log4cplus::MINUTELY, true, 10, false, false*//*, LOG4CPLUS_TEXT("yyyy-MM-dd")*/));
+	//appender2->setName(LOG4CPLUS_TEXT("DailyRollingFileAppender"));
 
+	//¿ØÖÆÌ¨
+	log4cplus::helpers::Properties properties;
+	properties.setProperty(LOG4CPLUS_TEXT("AllocConsole"), LOG4CPLUS_TEXT("true"));
+	properties.setProperty(LOG4CPLUS_TEXT("logToStdErr"), LOG4CPLUS_TEXT("false"));
+	properties.setProperty(LOG4CPLUS_TEXT("TextColor"), LOG4CPLUS_TEXT("1"));
+
+	//log4cplus::SharedAppenderPtr appender3(new log4cplus::Win32ConsoleAppender(true, false));
+	log4cplus::SharedAppenderPtr appender3(new log4cplus::Win32ConsoleAppender(properties));
+	appender3->setName(LOG4CPLUS_TEXT("Win32ConsoleAppender"));
+
+	//µ÷ÊÔ
+	log4cplus::SharedAppenderPtr appender4(new log4cplus::Win32DebugAppender);
+	appender4->setName(LOG4CPLUS_TEXT("Win32DebugAppender"));
 	//Layout
 	//log4cplus::Layout *simple_layout = new log4cplus::SimpleLayout;
 	//std::unique_ptr<Layout> simple_layout((Layout *)new log4cplus::SimpleLayout);
@@ -41,7 +58,8 @@ Widget::Widget(QWidget *parent)
 
 
 	logger.addAppender(appender1);
-	logger.addAppender(appender2);
+	//logger.addAppender(appender2);
+	logger.addAppender(appender4);
 	logger.setLogLevel(log4cplus::ALL_LOG_LEVEL);
 
 	LOG4CPLUS_INFO(logger, LOG4CPLUS_TEXT("info msg"));
