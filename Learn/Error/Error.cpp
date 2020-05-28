989c97ebf6b148ca4feced9661e4d9245bd9ace0
huangjxbq <519207704@qq.com>
2020-05-28 13:28:07 +0800

    错误处理

--- Learn/Error/Error.cpp
+++ Learn/Error/Error.cpp
@@ -1,20 +1,61 @@
-﻿// Error.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <Windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	LANGID langid = MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US);
+
+	//FORMAT_MESSAGE_ALLOCATE_BUFFER
+	{
+		//HLOCAL buffer = NULL;
+		//DWORD res = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		//	NULL, ERROR_SUCCESS, 0, (LPTSTR)&buffer, 0, NULL);
+
+		//if (0 == res)
+		//{
+		//	DWORD err = GetLastError();
+		//}
+		//else
+		//{
+		//	std::cout << (TCHAR*)(buffer) << std::endl;
+		//	LocalFree(buffer);
+		//}
+	}
+
+	//not set FORMAT_MESSAGE_ALLOCATE_BUFFER
+	{
+		//TCHAR buffer[256] = { 0 };
+
+		//DWORD res = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		//	NULL, ERROR_SUCCESS, langid, buffer, 256, NULL);
+
+		//if (0 == res)
+		//{
+		//	DWORD err = GetLastError();
+		//}
+		//else
+		//{
+		//	//std::cout << (TCHAR*)(buffer) << std::endl;
+		//}
+	
+	}
+
+
+	{
+		TCHAR buffer[256] = { 0 };
+		TCHAR source[256] = TEXT("自定义的");
+
+		DWORD res = FormatMessage(FORMAT_MESSAGE_FROM_STRING | FORMAT_MESSAGE_IGNORE_INSERTS,
+			source, ERROR_SUCCESS, langid, buffer, 256, NULL);
+
+		if (0 == res)
+		{
+			DWORD err = GetLastError();
+		}
+		else
+		{
+			//std::cout << (TCHAR*)(buffer) << std::endl;
+		}
+
+	}
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
