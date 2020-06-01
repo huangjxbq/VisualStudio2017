097b13b026b5856f051574bfba7a86f271dc2a38
huangjxbq <519207704@qq.com>
2020-06-01 18:17:58 +0800

    win32 字符串

---
+++ Learn/stringapiset/stringapiset.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	WCHAR source[64] = L"Windows核心编程";
+	CHAR *dst = NULL;
+
+	int requiredbytes = WideCharToMultiByte(CP_ACP, 0, source, -1, NULL, 0, NULL, NULL);
+
+	if (requiredbytes > 0)
+	{
+		dst = new CHAR[requiredbytes /*+ 1*/]{ 0 };
+
+		int res = WideCharToMultiByte(CP_ACP, 0, source, -1, dst, requiredbytes, NULL, NULL);
+
+	}
+	
+
+
+
+
+    std::cout << "Hello World!\n";
+}
